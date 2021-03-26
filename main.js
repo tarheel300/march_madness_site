abbc29f0f26874b5ba67e2e188f7ed25de0e3681
Guice <andypguice@gmail.com>
2021-03-26 00:03:12 -0400

    Populate Text Via JS
    Few updates across the files, biggest being populating a blank item by using JS logic.
    
    - Added the call in the HTML doc to handle adding jQuery functionality
    - Started using the $(document).ready(...) syntax
    - Started using "id" to be able to make use of modification of specific pieces
    - Got JS to populate a "<p>" tag with the current Date.

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ function my_first_function() {
     alert("Go Heels!")
 }
 
+function display_cur_ts() {
+    var current_time = new Date();
+    var current_year = current_time.getFullYear();
+    var current_month = ("0" + (current_time.getMonth() + 1)).slice(-2);
+    var current_date = ("0" + (current_time.getDate() + 1)).slice(-2);;
+    document.getElementById('cur_ts').innerHTML = "Current Date: " + current_year + "-" + current_month + "-" + current_date;
+}
+
+
+$(document).ready(display_cur_ts())
