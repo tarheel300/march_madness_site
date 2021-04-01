c1ad51a828a6294e3a554b0b4f3394fff27af890
tarheel300 <andypguice@gmail.com>
2021-04-01 00:02:11 -0400

    Slimming FIle Down to Just One Bracket

--- main.js
+++ main.js
@@ -24,70 +24,83 @@ function display_cur_ts() {
     document.getElementById('cur_ts').innerHTML = "Current Date: " + current_year + "-" + current_month + "-" + current_date;
 }
 
-function crt_bracket () {
-    //figure out how to run the matchups in the right order
-    //for now using scrappy way of hard-coding
-    var bracket_html = ''
-    var matchup_cnt = matchup_order.length
-    matchup_order.forEach(
-        function (item, index, array) {
-            cur_matchup = [[], []]
-            console.log(item, index, array);
-            if (item >= matchup_cnt / 2) {
-                console.log('Yes')
-                data.forEach(
-                    function (row, idx, row_data) {
-                        if (item == row['matchup']) {
-                            if (row['team_pos'] == 'bot') {
-                                cur_matchup[1].push(row['matchup'], row['seed'], row['team_nm'])
-                            }
-                            else {
-                                cur_matchup[0].push(row['matchup'], row['seed'], row['team_nm'])
-                            }
+function crt_bracket_col() {
+    var bracket_col_html = ''
+    var bracket_col_2_html = ''
+    var bracket_col_3_html = ''
+
+    //var rd_nbr = 1 //figure out how to make dynamic
+    //var my_data = data //does not truly copy the data, need to figure that out
+    var matchup_cnt = data.length
+
+    
+    for (i = 1; i < matchup_cnt; i++) {
+        if (i>= 4){
+            console.log(i)
+            cur_match = [[], []]
+            data.forEach(
+                function (row, idx, row_data) {
+                    //console.log(i, row);
+                    if (i == row['matchup']) {
+                        if (row['team_pos'] == 'bot') {
+                            //console.log('Here');
+                            cur_match[1].push(row['matchup'], row['seed'], row['team_nm'])
+                        }
+                        else {
+                            //console.log('There');
+                            cur_match[0].push(row['matchup'], row['seed'], row['team_nm'])
                         }
                     }
-                )
-                bracket_html += '<div class = "r1">'
-                bracket_html += '   <div class = "matchup" id = "' + cur_matchup[0][0] + '">'
-                bracket_html += '       <div class = "team" id = "' + cur_matchup[0][0] + 'Top">'
-                bracket_html += '           <div class = "seed"> ' + cur_matchup[0][1] + ' </div>'
-                //need parent id here......
-                bracket_html += '           <input type = "text" class = "team-name winner" onclick="select_winner(2)" value = "' + cur_matchup[0][2] + ' " readonly>'
-                bracket_html += '       </div>'
-
-                bracket_html += '       <div class = "team" id = "' + cur_matchup[1][0] + 'Bot">'
-                bracket_html += '           <div class = "seed"> ' + cur_matchup[1][1] + ' </div>'
-                //need parent id here......
-                bracket_html += '           <input type = "text" class = "team-name winner" onclick="select_winner(2)" value = "' + cur_matchup[1][2] + ' " readonly>'
-                bracket_html += '       </div>'
-                bracket_html += '    </div>' //close the matchup
-                bracket_html += '</div>' //close the round
-            }
-            else {
-                console.log('No');
-                console.log(matchup_cnt / item);
-                //need to determine the round number in a dynamic way
-                //starting with scrappy solution
-                var round_nbr = 2;
-                if (item == 1) {
-                    round_nbr = 3
                 }
-                bracket_html += '<div class = "r' + round_nbr + '">'
-                bracket_html += '    <div class = "matchup" id = "' + item + '">'
-                bracket_html += '        <div class = "team" id = "' + item + 'Top"></div>'
-                bracket_html += '        <div class = "team" id = "' + item + 'Bot"></div>'
-                bracket_html += '   </div>' //close the matchup
-                bracket_html += '</div>' //close the round
-            }
-            
+            )
+            console.log(cur_match[0], cur_match[1]);
+            bracket_col_html += '<div class = "r1">'
+            bracket_col_html += '   <div class = "matchup" id = "' + cur_match[0][0] + '">'
+            bracket_col_html += '       <div class = "team" id = "' + cur_match[0][0] + 'Top">'
+            bracket_col_html += '           <div class = "seed"> ' + cur_match[0][1] + ' </div>'
+            //need parent id here......
+            bracket_col_html += '           <input type = "text" class = "team-name" onclick="select_winner(2)" value = "' + cur_match[0][2] + ' " readonly>'
+            bracket_col_html += '       </div>'
+    
+            bracket_col_html += '       <div class = "team" id = "' + cur_match[1][0] + 'Bot">'
+            bracket_col_html += '           <div class = "seed"> ' + cur_match[1][1] + ' </div>'
+            //need parent id here......
+            bracket_col_html += '           <input type = "text" class = "team-name" onclick="select_winner(2)" value = "' + cur_match[1][2] + ' " readonly>'
+            bracket_col_html += '       </div>'
+            bracket_col_html += '    </div>' //close the matchup
+            bracket_col_html += '</div>' //close the round 
         }
-    )
-    console.log(bracket_html);
-    document.getElementById("bracket_two").innerHTML += bracket_html;
+        else {
+            var rd_nbr = 2
+            if (i == 1) {
+                rd_nbr = 3
+                bracket_col_3_html += '<div class = "r' + rd_nbr + '">'
+                bracket_col_3_html += '    <div class = "matchup" id = "' + i + '">'
+                bracket_col_3_html += '        <div class = "team" id = "' + i + 'Top"></div>'
+                bracket_col_3_html += '        <div class = "team" id = "' + i + 'Bot"></div>'
+                bracket_col_3_html += '   </div>' //close the matchup
+                bracket_col_3_html += '</div>' //close the round
+                console.log(bracket_col_3_html);
+            } else {
+                bracket_col_2_html += '<div class = "r' + rd_nbr + '">'
+                bracket_col_2_html += '    <div class = "matchup" id = "' + i + '">'
+                bracket_col_2_html += '        <div class = "team" id = "' + i + 'Top"></div>'
+                bracket_col_2_html += '        <div class = "team" id = "' + i + 'Bot"></div>'
+                bracket_col_2_html += '   </div>' //close the matchup
+                bracket_col_2_html += '</div>' //close the round
+                console.log(bracket_col_2_html);
+            }
+
+        }
+    }
+    //console.log(bracket_col_html);
+    document.getElementById("bracket").innerHTML += bracket_col_html;
+    document.getElementById("bracket").innerHTML += bracket_col_2_html;
+    document.getElementById("bracket").innerHTML += bracket_col_3_html;
 }
 
 function select_winner(dest_id) {
-    console.log("Hello!");
+    //console.log("Hello!");
     //get ID of the input clicked
     //check if the destination already has text
     dest_id_content = document.getElementById(dest_id).innerHTML;
@@ -95,7 +108,7 @@ function select_winner(dest_id) {
     console.log(dest_id_content.length)
 
     if (dest_id_content.length > 0) {
-        console.log("I'm Not Zero.")
+        //console.log("I'm Not Zero.")
         document.getElementById(dest_id).innerHTML = ''
     }
     //update the css classes of the input clicked (seed / name / score)
@@ -112,7 +125,7 @@ function select_winner(dest_id) {
 
 function on_load() {
     display_cur_ts();
-    crt_bracket();
+    crt_bracket_col();
 }
 
 $(document).ready(on_load())
